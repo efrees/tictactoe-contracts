cc28fbcdf8a843c10858b5284c2ee35cc46b8b40
Eric Frees <efrees@atlasrfid.com>
2019-02-12 18:38:15 -0600

    Add representations of initial contracts

---
+++ CellStates.cs
@@ -0,0 +1,9 @@
+namespace TicTacToe.Contracts
+{
+    public class CellStates
+    {
+        public static readonly string X = "X";
+        public static readonly string O = "O";
+        public static readonly string Blank = "-";
+    }
+}
---
+++ GameState.cs
@@ -0,0 +1,8 @@
+namespace TicTacToe.Contracts
+{
+    public class GameState
+    {
+        public string[] Board { get; set; }
+        public string NextPlayer { get; set; }
+    }
+}
